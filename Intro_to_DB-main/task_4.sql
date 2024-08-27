fb00fd83953f246e243d32ed7e138748c099c672
ahmed-a-nasser <a.abdelnasser000@gmail.com>
2024-08-27 10:05:10 +0300

    upload-is-done
    fun-project

---
+++ Intro_to_DB-main/task_4.sql
@@ -0,0 +1,17 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    CHARACTER_MAXIMUM_LENGTH AS 'Character Max Length',
+    NUMERIC_PRECISION AS 'Numeric Precision',
+    NUMERIC_SCALE AS 'Numeric Scale'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
